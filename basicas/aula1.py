7a8dcd1a2c3eb47eded5c9751968899633821f9b
Maicon Gomes <maicon.gomes0266@gmail.com>
2024-11-25 23:16:04 -0300

    upando basicas

---
+++ basicas/aula1.py
@@ -0,0 +1,16 @@
+"""
+DocString
+E escrever o que eu
+quiser
+
+asdfasdfd
+"""
+'''
+Usar para escrever suas notas
+'''
+
+
+# Permite escrever um comentário
+print(123) # Na Frente
+# Abaixo
+print(456)
