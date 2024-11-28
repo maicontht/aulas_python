5f4aeef7cc8274250e0c02ae0636bd15bd52fd16
Maicon Gomes <maicon.gomes0266@gmail.com>
2024-11-27 21:27:57 -0300

    Dia 2 aula

---
+++ basicas/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Maicon'
+sobrenome = 'Antonio'
+idade = 23
+ano_nascimento = 2001
+maior_de_idade = idade >= 18
+altura_metros = 1.7
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('é maior de idade:', maior_de_idade)
+print('Altura em metros:', altura_metros)
