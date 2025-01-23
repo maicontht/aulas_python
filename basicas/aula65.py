13bfb053f690e90e9afde8c77657396776ff5c8d
Maicon Gomes <maicon.gomes0266@gmail.com>
2025-01-22 23:21:10 -0300

    Mais um dia

---
+++ basicas/aula65.py
@@ -0,0 +1,12 @@
+"""
+CPF 746.824.890-70
+Colete a soma dos 9 primeiros digitos do CPF
+multiplicando cada um dos valores por um a 
+contagem regressiva começando de 10
+
+Ex.: 746.824.890-70 (746824890)  
+   10  9  8  7  6  5  4  3  2
+   7   4  6  8  2  4  8  9  0
+
+
+"""
