abc69f861aa0cc7a4029d8e7ac4951c757dbaf7c
Maicon Gomes <63738170+maiconmg@users.noreply.github.com>
2025-06-27 16:42:43 -0300

    lets go

---
+++ intermediarias/aula69.py
@@ -0,0 +1,18 @@
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+#
+# Como criar ambientes virtuais
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
+
+
+# Abra a pasta do seu projeto no terminal
+# e digite:
+# python -m venv venv
+#
+# Ativando e desativando meu ambiente virtual
+# Windows: .\venv\Scripts\activate
+# Linux e Mac: source venv/bin/activate
+# Desativar: deactivate
