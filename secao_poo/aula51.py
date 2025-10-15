74ee6373f37fc624b9120c8c9e5717d8be770528
Maicon Gomes <63738170+maiconmg@users.noreply.github.com>
2025-10-15 16:51:09 -0300

    Exercicio Sistema bancario v 0.1

--- secao_poo/aula51.py
+++ secao_poo/aula51.py
@@ -17,13 +17,13 @@ Banco
     Banco -> Conta
 
 Dicas:
-Criar classe Cliente que herda da classe Pessoa (Herança)
-    Pessoa tem nome e idade (com getters)
+Criar classe Cliente que herda da classe Pessoa (Herança) X
+    Pessoa tem nome e idade (com getters) x
     Cliente TEM conta (Agregação da classe ContaCorrente ou ContaPoupanca)
-Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
-    ContaCorrente deve ter um limite extra
-    Contas têm agência, número da conta e saldo
-    Contas devem ter método para depósito
+Criar classes ContaPoupanca e ContaCorrente que herdam de Conta X
+    ContaCorrente deve ter um limite extra X
+    Contas têm agência, número da conta e saldo x
+    Contas devem ter método para depósito 
     Conta (super classe) deve ter o método sacar abstrato (Abstração e
     polimorfismo - as subclasses que implementam o método sacar)
 Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
@@ -35,3 +35,100 @@ Banco será responsável autenticar o cliente e as contas da seguinte maneira:
 Só será possível sacar se passar na autenticação do banco (descrita acima)
 Banco autentica por um método.
 """
+
+# MY Resolution
+
+from abc import ABC, abstractmethod
+
+class Banco:
+    def __init__(self, banco):
+        self.banco = banco
+        self.clientes = []
+        self.contas = []
+
+class Conta(ABC):
+    def __init__(self,agencia):
+        self.agencia = agencia
+        self.conta = None
+        self.saldo = None
+        self.deposito = None
+
+    @property
+    @abstractmethod
+    def deposito(self):
+        return self.saldo
+    
+    @deposito.setter
+    def deposito(self, valor_depositado):
+        if valor_depositado <= 0:
+            print('Valor tem que ser mais que zero')
+        else:
+            self.deposito += self.deposito
+
+
+
+
+
+class ContaPoupanca(Conta):
+    def __init__(self, agencia, conta, saldo):
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
+        self.limite_saque = 1000
+        self.deposito = None
+
+        def deposito(self):
+            return self.saldo
+    
+        @deposito.setter
+        def deposito(self, valor_depositado):
+            if valor_depositado <= 0:
+                print('Valor tem que ser mais que zero')
+            else:
+                self.deposito += self.deposito
+    
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo):
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
+        self.limite_saque = 1500
+        self.deposito = None
+
+        
+        def deposito(self):
+            return self.saldo
+    
+        @deposito.setter
+        def deposito(self, valor_depositado):
+            if valor_depositado <= 0:
+                print('Valor tem que ser mais que zero')
+            else:
+                self.deposito += self.deposito
+        
+
+class Pessoa(ABC):
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_idade(self):
+        return self.idade
+    
+    def set_idade(self, t_idade):
+        if t_idade > 18 and t_idade < 100:
+            self.idade = t_idade
+        else: 
+            print('idade invalida')        
+
+class Cliente(Pessoa):
+    ...
+
+p1 = Pessoa('maicon', 25)
+
+bradesco = Banco('Bradesco')
+itau = Banco('Itau')
+
+
+
