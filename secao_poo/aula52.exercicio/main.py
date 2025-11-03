ac2d622229785f8176bd3f59adcca0abfdeb25b0
Maicon Gomes <63738170+maiconmg@users.noreply.github.com>
2025-11-03 12:20:22 -0300

    secao poo finalizada

--- secao_poo/aula52.exercicio/main.py
+++ secao_poo/aula52.exercicio/main.py
@@ -8,31 +8,49 @@ Conta (ABC)
     ContaCorrente
     ContaPoupanca
 
-Pessoa (ABC)
+Pessoa
     Cliente
-        Clente -> Conta
+        Clente -> Conta (um para um ou um para muitos)
 
 Banco
     Banco -> Cliente
     Banco -> Conta
 
 Dicas:
-Criar classe Cliente que herda da classe Pessoa (Herança) X
-    Pessoa tem nome e idade (com getters) x
+Criar classe Cliente que herda da classe Pessoa (Herança)
+    Pessoa tem nome e idade (com getters)
     Cliente TEM conta (Agregação da classe ContaCorrente ou ContaPoupanca)
-Criar classes ContaPoupanca e ContaCorrente que herdam de Conta X
-    ContaCorrente deve ter um limite extra X
-    Contas têm agência, número da conta e saldo x
-    Contas devem ter método para depósito 
+Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
+    ContaCorrente deve ter um limite extra
+    Contas têm agência, número da conta e saldo
+    Contas devem ter método para depósito
     Conta (super classe) deve ter o método sacar abstrato (Abstração e
     polimorfismo - as subclasses que implementam o método sacar)
 Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
 Banco será responsável autenticar o cliente e as contas da seguinte maneira:
-    Banco tem contas e clentes (Agregação)
+    Banco tem contas e clientes (Agregação)
     * Checar se a agência é daquele banco
     * Checar se o cliente é daquele banco
     * Checar se a conta é daquele banco
 Só será possível sacar se passar na autenticação do banco (descrita acima)
-Banco autentica por um método.
+Banco autentica por um método (autenticar).
 """
+import contas
+import pessoas
+from banco import Banco
 
+c1 = pessoas.Cliente('Luiz', 30)
+cc1 = contas.ContaCorrente(111, 222, 0, 0)
+c1.conta = cc1
+c2 = pessoas.Cliente('Maria', 18)
+cp1 = contas.ContaPoupanca(112, 223, 100)
+c2.conta = cp1
+banco = Banco()
+banco.clientes.extend([c1, c2])
+banco.contas.extend([cc1, cp1])
+banco.agencias.extend([111, 222])
+
+if banco.autenticar(c1, cc1):
+    cc1.depositar(10)
+    c1.conta.depositar(100)
+    print(c1.conta)
